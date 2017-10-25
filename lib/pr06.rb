6d331d502d6f1cb45c6868e1c7e58dfc228c1bf6
Marco Antonio Maestre Mirabal <alu0100890709@ull.edu.es>
2017-10-25 11:59:04 +0100

    Creado la estructura con bundler

---
+++ lib/pr06.rb
@@ -0,0 +1,5 @@
+require "pr06/version"
+
+module Pr06
+  # Your code goes here...
+end
---
+++ spec/pr06_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Pr06 do
+  it "has a version number" do
+    expect(Pr06::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
