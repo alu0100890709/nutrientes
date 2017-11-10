57fdc8a915e8b5652162f72e94a3047201911f56
Marco Antonio Maestre Mirabal <alu0100890709@ull.edu.es>
2017-11-10 12:21:06 +0000

    Creacion la super clase de Alimento, llamad Grupo que contiene los tipos de comida

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -2,14 +2,54 @@
 
 
 
-class Alimento
 
-
+class Grupo
+  
  
-attr_accessor :nombre,:prt,:gli,:lp
 
- def initialize(nombre,proteina,glucido,lipido)
+attr_accessor :carnes,:pescado,:lacteos,:al_graso,:al_carbohidratos,:verduras, :frutas, :bebidas
+  def initialize(carnes,pescado,lacteos,al_graso,al_carbohidratos,verduras, frutas, bebidas)
     
+
+@carnes = carnes
+@pescado =pescado
+@lacteos = lacteos
+@al_graso = al_graso
+@al_carbohidratos = al_carbohidratos
+@verduras = verduras
+@frutas = frutas
+@bebidas = bebidas
+
+  end
+  
+  def to_s
+      
+  if @carnes==1
+    "grupo de carnes y derivado:"
+  elsif @pescado==1
+      "grupo de pescado y mariscos:"
+      elsif @lacteos==1
+      "grupo de huevos, lacteos y helados:"
+      elsif @al_graso==1
+       "(grupo de alimentos grasos )"
+       elsif @al_carbohidratos==1
+       "grupo de alimentos ricos en carbohidratos:"
+       elsif @frutas==1
+         "grupo de frutas:"
+           elsif @verduras==1
+         "grupo de verduras y hortalizas:"
+         else
+         "grupo de bebidas:"
+      end
+    end  
+end
+#Clase alimento que engloba a todas las otras pequeñas clases.
+class Alimento < Grupo
+
+
+ def initialize(nombre,proteina,glucido,lipido,carnes,pescado,lacteos,al_graso,al_carbohidratos,verduras, frutas, bebidas)
+    
+super(carnes,pescado,lacteos,al_graso,al_carbohidratos,verduras, frutas, bebidas)
 
 @nombre = nombre
 @prt =proteina
@@ -17,7 +57,21 @@ attr_accessor :nombre,:prt,:gli,:lp
 @lp = lipido
 
   end
+  
+   def to_s
+   
 
+    s = "( "
+
+    s << super.to_s        #encadenamiento (chaining)
+
+    s << " #{@nombre}: #{@prt} g de proteina,#{@gl} g de glúcido,#{@lp} g de lipidos )"
+
+    s
+
+  end
+
+  
     def get_prt
       @prt
   end 
@@ -33,15 +87,13 @@ attr_accessor :nombre,:prt,:gli,:lp
     def get_gl
       @gl
   end 
-
+  
+  
 def valor_energetico
     
    ( @prt * 4) + (@gl * 4) +(@lp * 9)
     
 end
-   def to_s
 
-    "(#{@nombre}: #{@prt} g de proteina,#{@gl} g de glúcido,#{@lp} g de lipidos )"
-
-  end  
 end
+
