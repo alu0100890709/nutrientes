b0128fbcbdbfc58861aef15da457387a4f2dc27b
Marco Antonio Maestre Mirabal <alu0100890709@ull.edu.es>
2017-11-23 19:42:17 +0000

    Metodo funcional del calculo del Indice glucemico

--- lib/pr06/alimento.rb
+++ lib/pr06/alimento.rb
@@ -1,56 +1,57 @@
 #Clase alimento que engloba a todas las otras pequeñas clases.
 
-
-
-
 class Grupo
   
 include Comparable 
+attr_accessor :tipo
+  def initialize(tipo)
+   
+   @tipo = tipo 
 
-attr_accessor :carnes,:pescado,:lacteos,:al_graso,:al_carbohidratos,:verduras, :frutas, :bebidas
-  def initialize(carnes,pescado,lacteos,al_graso,al_carbohidratos,verduras, frutas, bebidas)
-    
+end
 
-@carnes = carnes
-@pescado =pescado
-@lacteos = lacteos
-@al_graso = al_graso
-@al_carbohidratos = al_carbohidratos
-@verduras = verduras
-@frutas = frutas
-@bebidas = bebidas
-
-  end
+  
   
   def to_s
       
-  if @carnes==1
+  if @tipo=='carnes'
     "grupo de carnes y derivado:"
-  elsif @pescado==1
+  elsif @tipo == 'pescado'
       "grupo de pescado y mariscos:"
-      elsif @lacteos==1
+      elsif  @tipo == 'lacteos'
       "grupo de huevos, lacteos y helados:"
-      elsif @al_graso==1
+      elsif @tipo == 'al_graso'
        "(grupo de alimentos grasos )"
-       elsif @al_carbohidratos==1
+       elsif @tipo == 'al_carbohidratos'
        "grupo de alimentos ricos en carbohidratos:"
-       elsif @frutas==1
+       elsif @tipo == 'frutas'
          "grupo de frutas:"
-           elsif @verduras==1
+            elsif @tipo == 'verduras'
          "grupo de verduras y hortalizas:"
          else
          "grupo de bebidas:"
       end
     end  
 end
+
+
+
+
 #Clase alimento que engloba a todas las otras pequeñas clases.
 class Alimento < Grupo
 
 
- def initialize(nombre,proteina,glucido,lipido,carnes,pescado,lacteos,al_graso,al_carbohidratos,verduras, frutas, bebidas)
-    
-super(carnes,pescado,lacteos,al_graso,al_carbohidratos,verduras, frutas, bebidas)
 
+attr_accessor :nombre,:prt,:gli,:lp,:ind_gluc
+
+
+ def initialize(nombre,proteina,glucido,lipido,tipo)
+    
+super(tipo)
+
+ 
+    
+@ind_gluc = nil  #variable para la glucosa
 @nombre = nombre
 @prt =proteina
 @gl = glucido
@@ -107,5 +108,32 @@ def <=>(other)
     
   end
 
+def calculate_index(data, gluc)
+        aibc = [[],[]]
+        data.each do |x|
+            aux = 0
+            x.each_index do |a| 
+                if (a  != 0) 
+                    aux += (((x[a] - x[0]) + (x[a - 1] - x[0])) /2)*5
+                end 
+            end 
+            aibc[0] << aux
+        end
+        gluc.each do |x|
+            aux = 0
+            x.each_index do |a| 
+                if (a  != 0) 
+                    aux += (((x[a] - x[0]) + (x[a - 1] - x[0])) /2)*5
+                end 
+            end 
+            aibc[1] << aux
+        end
+        ig_index = []
+        aibc[0].each_index { |x|  ig_index << ((aibc[0][x] / aibc[1][x]) * 100)}
+        suma = 0
+        ig_index.each{ |x| suma += x}
+        @ind_gluc = (suma / ig_index.length)
+
+    end
 end
 
