4c2b54cbfb528548da52222d43e2456fac4ebb55
Marco Antonio Maestre Mirabal <alu0100890709@ull.edu.es>
2017-11-23 19:27:33 +0000

    Pruebas del cacluo del Indice Glucemico

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -10,21 +10,26 @@ RSpec.describe Alimento do
 
   before :each do
 
-    @huevo = Alimento.new('Huevo Frito',14.1,0.0,19.5,0,0,1,0,0,0,0,0)
+    @huevo = Alimento.new('Huevo Frito',14.1,0.0,19.5,'lacteos')
    
-    @leche = Alimento.new('Leche',3.3,4.8,3.2,0,0,1,0,0,0,0,0)
+    @leche = Alimento.new('Leche',3.3,4.8,3.2,'lacteos')
    
-    @cerdo = Alimento.new('Cerdo',21.5,0.0,6.3,1,0,0,0,0,0,0,0)
+    @cerdo = Alimento.new('Cerdo',21.5,0.0,6.3,'carnes')
 
-    @bacalao = Alimento.new('Bacalao',17.7,0.0,0.4,0,1,0,0,0,0,0,0)
+    @bacalao = Alimento.new('Bacalao',17.7,0.0,0.4,'pescado')
 
-    @aceite = Alimento.new('Aceite',0.0,0.2,99.6,0,0,0,1,0,0,0,0)
+    @aceite = Alimento.new('Aceite',0.0,0.2,99.6,'al_graso')
 
-    @azucar = Alimento.new('Azúcar',0.0,99.8,0.0,0,0,0,0,1,0,0,0)
+    @azucar = Alimento.new('Azúcar',0.0,99.8,0.0,'al_carbohidratos')
 
-    @tomate = Alimento.new('Tomate',1.0,3.5,0.2,0,0,0,0,0,1,0,0)
+    @tomate = Alimento.new('Tomate',1.0,3.5,0.2,'verduras')
 
-    @platano = Alimento.new('Plátano',1.2,21.4,0.2,0,0,0,0,0,0,1,0)
+    @platano = Alimento.new('Plátano',1.2,21.4,0.2,'frutas')
+    
+    
+    #Variable usada para la practica 9 del calculo del indice glucemico
+    @manzana = Alimento.new('Manzana',1.0,1.0,1.0,'frutas')
+
 
 
   end
@@ -133,7 +138,7 @@ RSpec.describe Alimento do
 
   end
 
-describe "# Obtener clase del objeto " do
+  describe "# Obtener clase del objeto " do
 
     it "Clase de objeto" do
           
@@ -143,12 +148,12 @@ describe "# Obtener clase del objeto " do
 
   end
 
-describe "# Obtener jerarquia del objeto " do
+  describe "# Obtener jerarquia del objeto " do
 
     it "jerarquia de objeto" do
 
        
-        expect(@leche.lacteos).to be_a_kind_of(Grupo)
+        expect(@leche.tipo).to be_a_kind_of(Grupo)
     end
 
   end
@@ -169,5 +174,23 @@ describe "# Obtener jerarquia del objeto " do
         expect(@leche >= @aceite).to be(true)
     end
   end
-  
+   describe "# Obtener indice gluceimo " do
+
+    it "Observar que tiene la variable" do
+         
+        expect(@manzana).to have_attributes(:indexGluc => nil)
+    end
+
+    it "Metodo para el calculo del Indice Glucemico" do
+          
+        datos = [[6.7, 6.5, 6.8, 6.9, 7.0, 7.1, 6.9, 6.9, 6.9, 6.7, 6.9, 7.3, 7.0, 7.0, 7.2, 7.1, 6.8, 7.2 ,7.3 ,7.0 ,6.8 ,6.7, 6.8, 6.7, 6.9],[4.6 ,4.8 ,5.3, 5.6, 6.1, 6.5, 6.6, 7.0, 7.0, 6.8, 6.4, 6.3, 6.1, 6.1, 6.2, 6.0, 6.1, 6.1 ,6.2, 6.3, 6.4 ,6.1 ,6.1 ,5.7 ,5.9]]
+        gluc = [[4.9 ,5.3 ,5.9 ,6.7 ,7.2 ,7.6 ,8.0 ,8.2, 8.2 ,8.4 ,8.3 ,8.3 ,8.0 ,7.5, 7.1, 6.8 ,6.8 ,6.9, 6.8, 6.3 ,6.2 ,6.3 ,6.2 ,6.3, 6.1],[6.3, 5.4, 5.6, 5.7, 6.5, 7.4, 7.9, 7.4, 7.7, 7.9, 7.9, 7.8, 7.8, 7.8, 8.0, 8.5, 9.4 ,10.8 ,10.5, 9.1, 8.9, 8.3, 7.7, 7.6 ,7.5]]
+          @manzana.calculate_index(datos,gluc)
+            expect(@manzana.indexGluc).to eq(54.499780077077745)
+    end
+    
+ 
   end
+
+
+end
